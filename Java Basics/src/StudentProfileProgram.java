0ae90be40fe33e227a68536351dbff9500d5c6be
Raman-120 <ramanchaulagain059@gmail.com>
2026-08-01 16:38:55 +0545

    Student Profile Program along with it's readme

---
+++ Java Basics/src/StudentProfileProgram.java
@@ -0,0 +1,16 @@
+public class StudentProfileProgram {
+    public static void main(String[] args){
+
+        System.out.println("Name: Raman Chaulagain");
+        System.out.println("Email: ramanchaulagain059@gmail.com");
+        System.out.println("City: Hetauda, Makwanpur(Nepal)");
+        System.out.println("Internship track: Java");
+        System.out.println();
+        System.out.println("Hello sir, My name is Raman Chaulagain.\n" +
+                            "I am an enthusiast BCA student currently in second sem.\n" +
+                            "I am a passionate learner and I am always ready to learn new topics. ");
+
+
+    }
+
+}
--- out/production/Java Basics/README.md
+++ out/production/Java Basics/README.md
@@ -1,1 +1,7 @@
-This program demonstrates the basic Student Information 
+Student Information System: 
+This program demonstrates the basic Student Information
+such as student's name and age, and also it takes 5 
+subjects marks as an input and then displays the obtained
+percentage, obtained marks and result of the student.
+
+
