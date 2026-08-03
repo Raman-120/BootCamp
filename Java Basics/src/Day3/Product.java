7ec8823fc27fc30b0e55afc7dce532f9f7d858cb
Raman-120 <ramanchaulagain059@gmail.com>
2026-08-03 17:38:51 +0545

    set up the product class

---
+++ Java Basics/src/Day3/Product.java
@@ -0,0 +1,59 @@
+package Day3;
+
+public class Product {
+
+    private int productId;
+    private String name;
+    private String category;
+    private double price;
+    private int quantity;
+
+    Product(int productId, String name, String category, double price, int quantity){
+        this.productId = productId;
+        this.name = name;
+        this.category = category;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public int getProductId(){
+        return this.productId;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getCategory(){
+        return this.category;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public int getQuantity(){
+        return this.quantity;
+    }
+
+    public void getName(String name){
+        this.name = name;
+    }
+
+    public void setCategory(String category){
+        this.category = category;
+    }
+
+    public void setPrice(double price){
+        this.price = price;
+    }
+
+    public void setQuantity(int quantity){
+        this.quantity = quantity;
+    }
+
+    public String toString(){
+        return productId + " " + name + " " + category + " " + price + " " + quantity;
+    }
+
+}
