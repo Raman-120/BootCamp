24318af4d2cc699e1674ddd5070d02460eb5cd08
Raman-120 <ramanchaulagain059@gmail.com>
2026-08-03 14:06:01 +0545

    Initialized student class with a constructor

--- Java Basics/src/Student.java
+++ Java Basics/src/Student.java
@@ -1,2 +1,18 @@
 public class Student {
+
+    int studentID;
+    String name;
+    String email;
+    int age;
+    String course;
+
+    Student(int studentID, String name, String email, int age, String course){
+        this.studentID = studentID;
+        this.name = name;
+        this.email = email;
+        this.age = age;
+        this.course = course;
+    }
+
+
 }
