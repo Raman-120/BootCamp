bf79f2f47206482ec8cf44fccdf27b4d753b16d4
Raman-120 <ramanchaulagain059@gmail.com>
2026-08-03 18:09:13 +0545

    used List method and added two funtional features

---
+++ Java Basics/out/production/Java Basics/Day2/README.md
@@ -0,0 +1,11 @@
+Student Management System:
+<br>
+Day2.Student Management System is a simple menu based Management
+system that allows users to add, search and display students.
+I have used 3 classes, one is Day2.SimpleStudentManagementSystem
+that acts as a main class of our program and then I used
+Day2.Student class to collect the necessary information of the
+student such as student Id, name, email, course and age and
+then with the Day2.StudentManagement class I did the necessary
+operations such as adding, displaying or searching students
+through appropriate methods.
--- Java Basics/src/Day3/ProductManagementSystem.java
+++ Java Basics/src/Day3/ProductManagementSystem.java
@@ -1,4 +1,90 @@
 package Day3;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import  java.util.Scanner;
+
 public class ProductManagementSystem {
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        List<Product> electronics = new ArrayList<>();
+        List<Product> clothing = new ArrayList<>();
+        Iterator<Product> i;
+        Product product;
+
+
+
+        int choice = 0;
+
+        do{
+            System.out.println("*******************************");
+            System.out.println("1. Add product");
+            System.out.println("2. View all products");
+            System.out.println("3. Search product by ID");
+            System.out.println("4. Update product");
+            System.out.println("5. Delete product");
+            System.out.println("6. Search Product by Category");
+            System.out.println("7. Exit");
+            System.out.println("*******************************");
+
+            System.out.print("Enter your choice: ");
+            choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choice){
+                case 1 -> {
+                    System.out.print("Enter the product ID: ");
+                    int productId = scanner.nextInt();
+                    scanner.nextLine();
+
+                    System.out.print("Enter the product name: ");
+                    String productName = scanner.nextLine();
+
+                    System.out.print("Enter the product Category: ");
+                    String productCategory = scanner.nextLine().toLowerCase();
+
+                    System.out.print("Enter the product price: ");
+                    double productPrice = scanner.nextDouble();
+                    scanner.nextLine();
+
+                    System.out.print("Enter the product quantity: ");
+                    int productQuantity = scanner.nextInt();
+                    scanner.nextLine();
+
+                    if(productCategory.equals("electronics")){
+                        electronics.add(new Product(productId, productName,productCategory, productPrice, productQuantity));
+                    }
+                    else{
+                        clothing.add(new Product(productId, productName,productCategory, productPrice, productQuantity));
+                    }
+                }
+
+                case 2 -> {
+                    //view all products of electronics category
+                    System.out.println("******************");
+                    i = electronics.iterator();
+                    while (i.hasNext()){
+                        product = i.next();
+                        System.out.println(product);
+                    }
+
+                    // view all products of clothing category
+                    System.out.println("******************");
+                    i = clothing.iterator();
+                    while (i.hasNext()){
+                        product = i.next();
+                        System.out.println(product);
+                    }
+
+                }
+                default -> System.out.println("Please choose a valid option.");
+            }
+
+
+        }while (choice != 7);
+
+    }
 }
